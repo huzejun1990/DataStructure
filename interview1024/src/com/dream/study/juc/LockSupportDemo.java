0917a3b0ced17fae4ed2d14aae3523f5c7187c91
huzejun <961639130@qq.com>
2020-11-11 18:08:05 +0800

    AQS 可重入锁

--- interview1024/src/com/dream/study/juc/LockSupportDemo.java
+++ interview1024/src/com/dream/study/juc/LockSupportDemo.java
@@ -1,6 +1,10 @@
 package com.dream.study.juc;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by huzejun
@@ -9,9 +13,67 @@ import java.util.concurrent.TimeUnit;
 public class LockSupportDemo {
 
     static Object objectLock = new Object();
+    static Lock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
 
     public static void main(String[] args) {
 
+        Thread a = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "\t" + "come in" + System.currentTimeMillis());
+            LockSupport.park(); //wait 被阻塞，等待通知等待放行，它要通过需要许可证
+            System.out.println(Thread.currentThread().getName() + "\t" + "---被唤醒--" + System.currentTimeMillis());
+        }, "a");
+        a.start();
+
+        //暂停几秒钟线程
+      /*  try {
+            TimeUnit.SECONDS.sleep(3L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        Thread b = new Thread(() -> {
+            LockSupport.unpark(a);
+            System.out.println(Thread.currentThread().getName() + "\t" + "---被唤醒--");
+
+        }, "b");
+        b.start();
+
+    }
+
+    private static void lockAwaitSignal() {
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t" + "----come in");
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "\t" + "----被唤醒");
+            } finally {
+                lock.unlock();
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                condition.signal();
+                System.out.println(Thread.currentThread().getName() + "\t" + "----通知---");
+            } finally {
+                lock.unlock();
+            }
+        }, "B").start();
+    }
+
+    private static void synchronizedWaitNotify() {
         new Thread(() -> {
             //暂停几秒钟线程
             try {
@@ -19,24 +81,23 @@ public class LockSupportDemo {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            synchronized (objectLock){
-                System.out.println(Thread.currentThread().getName()+"\t"+"---come in--");
+            synchronized (objectLock) {
+                System.out.println(Thread.currentThread().getName() + "\t" + "---come in--");
                 try {
                     objectLock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName()+"\t"+"-----被唤醒-----");
+                System.out.println(Thread.currentThread().getName() + "\t" + "-----被唤醒-----");
             }
 
-        },"A").start();
+        }, "A").start();
 
         new Thread(() -> {
-            synchronized (objectLock){
+            synchronized (objectLock) {
                 objectLock.notify();
-                System.out.println(Thread.currentThread().getName()+"\t"+"---通知---");
+                System.out.println(Thread.currentThread().getName() + "\t" + "---通知---");
             }
-        },"B").start();
-
+        }, "B").start();
     }
 }
